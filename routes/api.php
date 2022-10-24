52a84ff277e7b6ba1b1a13c18a274b10cfe4e73f
Fahriza13 <fahrizafatah0@gmail.com>
2022-10-24 11:09:14 +0700

    Menambah book

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+/*
+* Task
+* URL : http://localhost:8000/api/halo
+* Method: GET
+* Exec: function
+* Return: JSON => {"me": "Ganteng"}
+// */
+// Route::get('halo', function(){
+//     return ["me" => "ganteng"];
+// });
+
+/*
+* Task
+* URL : http://localhost:8000/api/halo
+* Method: GET
+* Exec: function
+* Return: JSON => {"me": "Ganteng"}
+*/
+Route::resource('hellocontroller', HelloController::class);
+Route::resource('siswa', SiswaController::class);
+Route::resource('book', BookController::class);
